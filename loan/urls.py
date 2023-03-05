b0e64634f6ae1e76884db91f7ddf3dd95a5cc399
PhytonKing <reachcreditshare@creditshare.com>
2023-03-05 10:01:18 -0800

    Login/Registration system made

--- loan/urls.py
+++ loan/urls.py
@@ -1,7 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from loan.views import *
 app_name = "loan"
 urlpatterns = [
     path("", index, name="index"),
+    path("login", login_view, name="login_view"),
+    path("register", register_view, name="register"),
+    path("logout", logout_view, name="logout")
+    
 ]
--- loan/views.py
+++ loan/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, logout
 from loan.models import *
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 # Create your views here.
-def index(request):
-    return render(request, "loan/login.html")
 
-def login(request):
+@login_required(login_url="/loan/login")
+def index(request):
+    return render(request, "loan/index.html")
+
+def login_view(request):
     if request.method == "GET":
         return render(request, "loan/login.html")
     else:
@@ -15,13 +20,28 @@ def login(request):
         user = authenticate(username=u, password=p)
         if user is not None:
             login(request, user)
-            return render(request, "loan/index.html")
+            return HttpResponseRedirect(reverse("loan:index"))
         else:
-            return render(request, "loan/index.html")
+            return render(request, "loan/login.html", {"message": "incorrect username or password"})
 
-def register(request):
+def register_view(request):
     if request.method == "GET":
-        return render(request, "loan/login.html")
+        return render(request, "loan/register.html")
+    else:
+        u = request.POST["username"]
+        p = request.POST["password"]
+        password_match = request.POST["password"] == request.POST["c_password"]
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        email = request.POST["email"]
+        phone_number = request.POST["tel"]
+        verify = request.POST["verify"]
+        if verify and password_match:
+            l = User(username=u, password=p, email=email, first_name=first_name, last_name=last_name)
+            l.save()
+            return HttpResponseRedirect(reverse("loan:index"))
+        else:
+            return render(request, "loan/register.html", {"message": "Register did not go through, check registration details."})
 
 def logout_view(request):
     logout(request)
